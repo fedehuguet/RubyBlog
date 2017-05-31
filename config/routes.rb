7d527565a3a30ad5fb8f4d77f03368f3d0bd665c
fedehuguet <fede.huguet14@gmail.com>
2017-05-31 18:59:40 -0500

    Before layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  get 'pages/resources'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   root :to => "home#index"
   resources :catergories
   resources :posts
+  match '/about', :to => 'pages#about', via: [:get, :post]
+  match '/contact', :to => 'pages#contact', via: [:get, :post]
+  match '/resources', :to => 'pages#resources', via: [:get, :post]
+
 
   ActiveAdmin.routes(self)
 end
