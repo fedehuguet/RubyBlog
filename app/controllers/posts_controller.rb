41f103ca252f3bf6a91df35cf981912e92436a60
fedehuguet <fede.huguet14@gmail.com>
2017-06-01 15:45:04 -0500

    Ransack search

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
+
 	def index
-		@posts = Post.all
+		@q = Post.ransack(params[:q])
+		@posts = @q.result(distinct: true)
 	end
 	def new
 		@post = Post.new
